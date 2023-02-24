c031f2c42d059a8f792af4d3f0f92c0641e35207
btschwartz12 <105884019+btschwartz12@users.noreply.github.com>
2023-02-24 17:17:02 +0000

    Build: (3fc6388) Test branch

---
+++ sql/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO dogs (name, age, skills, picture_url)
+VALUES ('Rowdy', 4, 'Python, SQL, Git', 'https://example.com/rowdy.jpg');
+
+INSERT INTO dogs (name, age, skills, picture_url)
+VALUES ('Piper', 2, 'JavaScript, React, CSS', 'https://example.com/piper.jpg');
+
+INSERT INTO dogs (name, age, skills, picture_url)
+VALUES ('Seymour', 6, 'Java, Spring, Docker', 'https://example.com/seymour.jpg');
+
