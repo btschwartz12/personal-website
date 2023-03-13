2d24eb575c83b21afc3f310c96ca883f3107350c
Ben Schwartz <btschwartz12@gmail.com>
2023-03-13 18:14:54 +0000

    Trying webpack3

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,41 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+const publicUrl = "http://107.173.251.101"
 
 module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      filename: 'index.html',
+      PUBLIC_URL: publicUrl
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public',
+          to: '.',
+          globOptions: {
+            ignore: ['**/index.html']
+          }
+        }
+      ]
+    }),
+    new webpack.DefinePlugin({
+      'process.env.PUBLIC_URL': '/'
+    })
+  ],
+  
   module: {
     rules: [
+      
       {
         test: /\.(jsx|js)$/,
         include: path.resolve(__dirname, 'src'),
@@ -25,6 +52,7 @@ module.exports = {
           }
         }]
       },
+      
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
